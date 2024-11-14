ba7cb38ebb752e1e91481d46a4507fd6ceb4db44
vladsergmvs <72503462+vladsergmvs@users.noreply.github.com>
2024-11-14 14:34:17 +0000

    Deploying to gh-pages from @ vladsergmvs/goit-js-hw-11@85e2bd4a43bbd180579fbf81267ffebf0355a628 🚀

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-import"./assets/vendor-DJ7Tif3U.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&l(o)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function l(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const n="https://pixabay.com/api/",c="47047492-675b2ed8a2567f743231acd4f",u=document.querySelector(".search-form"),f=document.querySelector(".gallery-list");u.addEventListener("submit",m);function m(s){s.preventDefault();const i=s.target.elements.searchQuery.value;d(i).then(r=>{f.innerHTML=h(r)}).catch(r=>{console.log("catch",r)})}function d(s){const i=new URLSearchParams({key:c,q:s,image_type:"photo",orientation:"horizontal",safesearch:"true"});return fetch(`${n}?${c}&${i}`).then(r=>{if(!r.ok)throw new Error(r.statusText);return r.json()})}function h(s){return s.map(({largeImageURL:i,webformatURL:r,tags:l,likes:e,views:t,comments:o,downloads:a})=>`
-     <li class="gallery-card">
-   <a class="gallery-card-link" href="${i}">
-    <img class="gallery-image" src="${r}"alt="${l}"/>
-  </a>           
-            <ul class="img-list">
-            <li class="img-list-item">
-                <h3>Likes</h3>
-                <p>"${e}"</p>
-            </li>
-            <li class="img-list-item">
-                <h3>Views</h3>
-                <p>"${t}"</p>
-            </li>
-            <li class="img-list-item">
-                <h3>Comments</h3>
-                <p>"${o}"</p>
-            </li>
-            <li class="img-list-item">
-                <h3>Downloads</h3>
-                <p>"${a}"</p>
-            </li>
-            </ul>
-                
-</li>
-  `).join("")}
+import"./assets/vendor-Bgfrje-I.js";(function(){const l=document.createElement("link").relList;if(l&&l.supports&&l.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&a(s)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const n="https://pixabay.com/api/",o="47047492-675b2ed8a2567f743231acd4f";function u(i){const l=new URLSearchParams({key:o,q:i,image_type:"photo",orientation:"horizontal",safesearch:"true"});return fetch(`${n}?${o}&${l}`).then(r=>{if(!r.ok)throw new Error(r.statusText);return r.json()})}function f(i){return i.map(({largeImageURL:l,webformatURL:r,tags:a,likes:e,views:t,comments:s,downloads:c})=>`
+       <li class="gallery-list-item">
+     <a class="gallery-list-item-link" href="${l}">
+      <img class="gallery-image" src="${r}"alt="${a}"/>
+    </a>           
+              <ul class="gallery-card-list">
+              <li class="gallery-card-list-item">
+                  <h3>Likes</h3>
+                  <p>"${e}"</p>
+              </li>
+              <li class="gallery-card-list-item">
+                  <h3>Views</h3>
+                  <p>"${t}"</p>
+              </li>
+              <li class="gallery-card-list-item">
+                  <h3>Comments</h3>
+                  <p>"${s}"</p>
+              </li>
+              <li class="gallery-card-list-item">
+                  <h3>Downloads</h3>
+                  <p>"${c}"</p>
+              </li>
+              </ul>
+                  
+  </li>
+    `).join("")}const d=document.querySelector(".js-search-form"),m=document.querySelector(".js-gallery-list");d.addEventListener("submit",h);function h(i){i.preventDefault();const l=i.target.elements.searchQuery.value;u(l).then(r=>{m.innerHTML=f(r.hits)}).catch(r=>{console.log("catch",r)})}
 //# sourceMappingURL=index.js.map
