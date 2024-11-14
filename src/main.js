85e2bd4a43bbd180579fbf81267ffebf0355a628
Volodymyr <vladsergmvs@gmail.com>
2024-11-14 16:33:32 +0200

    cards

--- src/main.js
+++ src/main.js
@@ -2,12 +2,20 @@
 import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
-const BASE_URL = 'https://pixabay.com/api/',
-  API_KEY = '47047492-675b2ed8a2567f743231acd4f';
+// Описаний у документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-const form = document.querySelector('.search-form'),
-  galleryList = document.querySelector('.gallery-list');
+import { fetchData } from './js/pixabay-api';
+import { createMarkup } from './js/render-functions';
 
+//////////////////////////////////////////////////////
+
+const form = document.querySelector('.js-search-form'),
+  galleryList = document.querySelector('.js-gallery-list');
+
+//////////////////////////////////////////////////////
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
@@ -16,66 +24,9 @@ function handleSubmit(event) {
 
   fetchData(searchQuery)
     .then(data => {
-      galleryList.innerHTML = createMarkup(data);
+      galleryList.innerHTML = createMarkup(data.hits);
     })
     .catch(error => {
       console.log('catch', error);
     });
-}
-
-function fetchData(searchQuery) {
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: searchQuery,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  });
-  return fetch(`${BASE_URL}?${API_KEY}&${params}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  });
-}
-
-function createMarkup(array) {
-  return array
-    .map(
-      ({
-        largeImageURL,
-        webformatURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => `
-     <li class="gallery-card">
-   <a class="gallery-card-link" href="${largeImageURL}">
-    <img class="gallery-image" src="${webformatURL}"alt="${tags}"/>
-  </a>           
-            <ul class="img-list">
-            <li class="img-list-item">
-                <h3>Likes</h3>
-                <p>"${likes}"</p>
-            </li>
-            <li class="img-list-item">
-                <h3>Views</h3>
-                <p>"${views}"</p>
-            </li>
-            <li class="img-list-item">
-                <h3>Comments</h3>
-                <p>"${comments}"</p>
-            </li>
-            <li class="img-list-item">
-                <h3>Downloads</h3>
-                <p>"${downloads}"</p>
-            </li>
-            </ul>
-                
-</li>
-  `
-    )
-    .join('');
 }
